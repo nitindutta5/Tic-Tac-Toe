b65a5e5f0e73d925e2f4c0ca68d829a09fd18faf
Nitin Dutta <nitindutta5@gmail.com>
2019-11-23 21:14:54 +0530

    added js

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,27 @@
-// $(document).ready(function(){
-//     var c=0;
-//     $("td").click(function(){
-//         c++;
-//         if(c%2==0)
-//       $(".cross").show();
-//     });
-//   });
-for (let i = 1; i <= 9; i++) {
+$(document).ready(function () {
+    var c = 0;
+    $("td").click(function () {
+        c++;
+        if (c % 2 == 0)
+            // var id = event.target.id;
+            $(".cross").show();
+        else
+            $(" .zero").show();
+    });
+});
 
-    // if (i % 2 == 0) {
-    //     $(document).ready(function () {
-    //         $("td").click(function () {
-    //             $("#(get_reply())").show(".cross");
-    //         });
-    //     });
 
-    // } else {
-    //     $(document).ready(function () {
-    //             $("td").click(function () {
-    //                     $("#(get_reply())".show(".cross");
-    //                     });
-    //             });
-    //     }
-    // }
+// } else {
+//     $(document).ready(function () {
+//             $("td").click(function () {
+//                     $("#(get_reply())".show(".cross");
+//                     });
+//             });
+//     }
+// }
 
-    function get_reply() {
-        var id = event.target.id;
-        console.log(id);
-        return id;
-    }
+function get_reply() {
+    var a = "#".concat(event.target.id);
+    console.log(event.target.id);
+    alert(a);
+}
