b55c07329e83766460d9905937f4ffa88c8fbbbf
Nitin Dutta <nitindutta5@gmail.com>
2019-11-25 16:26:39 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -3,12 +3,38 @@ $(document).ready(function () {
     $("td").click(function () {
         c++;
         var id = $(this).attr('id');
-        if (c % 2 == 0)
-            $("#" + id + " .zero").show();
-        else
-            $("#" + id + " .cross ").show();
+        if (c % 2 == 0) {
+            $("#" + id).html("<div class='zero'><img class='img-fluid' src='assets/zero.png'></div>");
+            $("#" + id).off('click');
+        } else {
+            $("#" + id).html("<div class='cross'><img class='img-fluid' src='assets/cross.jpeg'></div>");
+            $("#" + id).off('click');
+        }
+        if (($("#1 div").hasClass("zero") && $("#2 div").hasClass("zero") && $("#3 div").hasClass("zero")) ||
+            ($("#3 div").hasClass("zero") && $("#4 div").hasClass("zero") && $("#6 div").hasClass("zero")) ||
+            ($("#7 div").hasClass("zero") && $("#8 div").hasClass("zero") && $("#9 div").hasClass("zero")) ||
+            ($("#1 div").hasClass("zero") && $("#3 div").hasClass("zero") && $("#7 div").hasClass("zero")) ||
+            ($("#2 div").hasClass("zero") && $("#4 div").hasClass("zero") && $("#8 div").hasClass("zero")) ||
+            ($("#3 div").hasClass("zero") && $("#6 div").hasClass("zero") && $("#9 div").hasClass("zero")) ||
+            ($("#1 div").hasClass("zero") && $("#4 div").hasClass("zero") && $("#9 div").hasClass("zero")) ||
+            ($("#3 div").hasClass("zero") && $("#4 div").hasClass("zero") && $("#7 div").hasClass("zero"))) {
+            alert("ZERO WINS");
+
+        }
+        if (($("#1 div").hasClass("cross") && $("#2 div").hasClass("cross") && $("#3 div").hasClass("cross")) ||
+            ($("#3 div").hasClass("cross") && $("#4 div").hasClass("cross") && $("#6 div").hasClass("cross")) ||
+            ($("#7 div").hasClass("cross") && $("#8 div").hasClass("cross") && $("#9 div").hasClass("cross")) ||
+            ($("#1 div").hasClass("cross") && $("#3 div").hasClass("cross") && $("#7 div").hasClass("cross")) ||
+            ($("#2 div").hasClass("cross") && $("#4 div").hasClass("cross") && $("#8 div").hasClass("cross")) ||
+            ($("#3 div").hasClass("cross") && $("#6 div").hasClass("cross") && $("#9 div").hasClass("cross")) ||
+            ($("#1 div").hasClass("cross") && $("#4 div").hasClass("cross") && $("#9 div").hasClass("cross")) ||
+            ($("#3 div").hasClass("cross") && $("#4 div").hasClass("cross") && $("#7 div").hasClass("cross"))) {
+            alert("CROSS WINS");
+
+        }
 
     });
+
 });
 
 
